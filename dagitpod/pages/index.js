7ac83b51797ee5eecb145ff00c91ee2edc0bc89f
waveyouat <waveyouat@gmail.com>
2022-01-23 00:51:08 +0000

    render home props serverside

--- dagitpod/pages/index.js
+++ dagitpod/pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({name}) {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          {name}
         </h1>
 
         <p className={styles.description}>
@@ -66,4 +66,13 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+export async function getServerSideProps ({req, res}) {
+
+  return {
+    props: {
+      name: "gaige"
+    }
+  }
 }
